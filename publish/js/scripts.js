c144fd502755fd506eb23eacd75747eaa2c12a71
Laurie Fumarolo <pwdlaurie@gmail.com>
2015-03-19 20:33:37 -0500

    Fixing an issue where when user taps on project tile (on iOS devices), user isn't taken to project page. Instead pink corners show, but nothing else happens.

--- publish/js/scripts.js
+++ publish/js/scripts.js
@@ -1,18 +1,31 @@
 $(document).ready(function() {
-	// initially, hide the pink corners around each image tile
-	$('.top').hide();
-	$('.bottom').hide();
+	// helper function to hide pink corners around an input project tile
+	function hideCorners(projectTile) {
+		projectTile.find('.top').hide();
+		projectTile.find('.bottom').hide();
+	}
 
-	// when user hovers over an image tile,
-	// show pink corners
-	$('.tile_menu_item').hover(
-		function() {
-			$('.top').show();
-			$('.bottom').show();
-		},
-		function() {
-			$('.top').hide();
-			$('.bottom').hide();
-		}
-	);
+	// helper function to show pink corners around an input project tile
+	function showCorners(projectTile) {
+		projectTile.find('.top').show();
+		projectTile.find('.bottom').show();
+	}
+
+	// initially, hide the pink corners around each project tile
+	$('.project_tile').each(function() {
+		hideCorners($(this));
+	});
+
+	// when user hovers over a project tile, show pink corners
+	// using touchstart and mouseenter for iOS devices
+	// otherwise, when user touches tile, the pink corners will show
+	// but user won't be taken to the project page
+	$('.project_tile').on('touchstart mouseenter', function() {
+		showCorners($(this));
+	});
+
+	// when user moves away from a project tile, hide pink corners
+	$('.project_tile').on('touchmove mouseleave click', function() {
+		hideCorners($(this));
+	});
 });

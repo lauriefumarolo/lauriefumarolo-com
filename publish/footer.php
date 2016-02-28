3050b5f845eb68f50d7d85d55629f7df7a5f4280
Laurie Fumarolo <pwdlaurie@gmail.com>
2016-02-28 17:12:11 -0600

    Mobile friendly footer.

--- publish/footer.php
+++ publish/footer.php
@@ -1,10 +1,10 @@
 <footer>
 	<div class="flex-container">
 		<div class="flex-item">
-			<p>Copyright &copy;2016 Laurie Fumarolo. All Rights Reserved.</p>
+			<p><a href="acknowledgements.php">Acknowledgements</a></p>
 		</div>
 		<div class="flex-item">
-			<p><a href="acknowledgements.php">Acknowledgements</a></p>
+			<p>Copyright &copy;2016 Laurie Fumarolo. All Rights Reserved.</p>
 		</div>
 		<div class="flex-item">
 			<a href="http://linkedin.com/in/lauriefumarolo" target="_blank">

01e73e391da77294311672ccf583e13e613ef242
Laurie Fumarolo <pwdlaurie@gmail.com>
2016-03-01 22:06:36 -0600

    Footer IE9+

--- publish/footer.php
+++ publish/footer.php
@@ -1,12 +1,12 @@
 <footer>
-	<div class="flex-container">
-		<div class="flex-item">
-			<p><a href="acknowledgements.php">Acknowledgements</a></p>
-		</div>
-		<div class="flex-item">
+	<div class="container">
+		<div class="second">
 			<p>Copyright &copy;2016 Laurie Fumarolo. All Rights Reserved.</p>
 		</div>
-		<div class="flex-item">
+		<div class="first">
+			<p><a href="acknowledgements.php">Acknowledgements</a></p>
+		</div>
+		<div class="third">
 			<a href="http://linkedin.com/in/lauriefumarolo" target="_blank">
 				<span class="fa-stack fa-lg">
 					<i class="fa fa-circle fa-stack-2x"></i>

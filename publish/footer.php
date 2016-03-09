7c968a96395fe9fdd26ed1eb238a244a8f08089f
Laurie Fumarolo <pwdlaurie@gmail.com>
2016-03-08 21:18:27 -0600

    Bootstrap grid for footer.

--- publish/footer.php
+++ publish/footer.php
@@ -1,18 +1,20 @@
 <footer>
 	<div class="container">
-		<div class="second">
-			<p>Copyright &copy;2016 Laurie Fumarolo. All Rights Reserved.</p>
-		</div>
-		<div class="first">
-			<p><a href="acknowledgements.php">Acknowledgements</a></p>
-		</div>
-		<div class="third">
-			<a href="http://linkedin.com/in/lauriefumarolo" target="_blank">
-				<span class="fa-stack fa-lg">
-					<i class="fa fa-circle fa-stack-2x"></i>
-					<i class="fa fa-linkedin fa-stack-1x social-logo"></i>
-				</span>
-			</a>
+		<div class="row">
+			<div class="col-sm-8 col-sm-push-2 footer_text">
+				<p><span class="copywrap">Copyright &copy;2016 Laurie Fumarolo.</span> <span class="copywrap">All Rights Reserved.</span></p>
+			</div>
+			<div class="col-sm-2 col-sm-pull-8 footer_text">
+				<p><a href="acknowledgements.php">Acknowledgements</a></p>
+			</div>
+			<div class="col-sm-2">
+				<a href="http://linkedin.com/in/lauriefumarolo" target="_blank">
+					<span class="fa-stack fa-lg">
+						<i class="fa fa-circle fa-stack-2x"></i>
+						<i class="fa fa-linkedin fa-stack-1x social-logo"></i>
+					</span>
+				</a>
+			</div>
 		</div>
 	</div>
 </footer>

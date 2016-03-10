1209d9f3af0f8932c680e357df5afc33f7f59b19
Laurie Fumarolo <pwdlaurie@gmail.com>
2016-03-09 20:49:58 -0600

    jQuery from CDN.

--- publish/head_includes.php
+++ publish/head_includes.php
@@ -6,7 +6,7 @@
 <!--[if lt IE 9]>
 <script src="js/html5shiv.min.js"></script>
 <![endif]-->
-<script src="js/jquery-1.12.0.min.js" type="text/javascript"></script>
+<script src="//code.jquery.com/jquery-1.12.1.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
 <script>

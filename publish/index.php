5e3883af5e095a0b27b16f2565458a0c848fd3c1
Laurie Fumarolo <pwdlaurie@gmail.com>
2016-10-05 22:16:55 -0500

    1. Move planetarium and jBaquery projects to one row on desktop, removing grey row. 2. Lighter footer. 3. Cleanup.

--- publish/index.php
+++ publish/index.php
@@ -25,10 +25,10 @@
 	</div>
 	<div id="content" class="container">
 		<div class="row">
-			<div class="col-sm-5 col-md-4 vcenter img_row">
+			<div class="col-md-4 vcenter img_row">
 				<img src="images/rca_screenshot.png" alt="RCA Modular" />
 			</div>
-			<div class="col-sm-7 col-md-8 vcenter text_row">
+			<div class="col-md-8 vcenter text_row">
 				<h2>Robotic Construction Automation, LLC</h2>
 				<p>I implemented this site for RCA, LLC, a company that will be offering a new concept for constructing multi-story, multi-dwelling facilities. I was hired as a web developer to build this new site based on the client's design.</p>
 				<p>On the homepage, I utilized the Bootstrap grid to fulfill the client's requirement for a left-right, snaking flow chart on larger screens, but adapt to a purely vertical layout on mobile devices.</p>
@@ -36,11 +36,12 @@
 				<p><a href="http://www.rca-modular.com" target="_blank">View the Site</a></p>
 			</div>
 		</div>
-		<div class="row grey_row">
-			<div class="col-sm-5 col-sm-push-7 col-md-4 col-md-push-8 vcenter img_row">
+		<hr />
+		<div class="row double_row border_row">
+			<div class="img_row">
 				<img src="images/cityplanetarium_screenshot.png" alt="City Planetarium" />
 			</div>
-			<div class="col-sm-7 col-sm-pull-5 col-md-8 col-md-pull-4 vcenter text_row">
+			<div class="text_row">
 				<h2>City Planetarium</h2>
 				<p>I created this site to demonstrate my knowledge of Twitter's Bootstrap framework and other modern web design trends. Here, I have included parallax scrolling and a large image banner on the homepage. The transparent menubar over this banner makes quite a statement. To maintain readability, however, I have written JavaScript to remove the effect as the user scrolls down the page.</p>
 				<p>This responsive site, for a fictional planetarium, is built on HTML, CSS, JavaScript, jQuery, and Bootstrap.</p>
@@ -48,11 +49,12 @@
 				<p><a href="http://bitbucket.org/lcaires2/cityplanetarium" target="_blank">View the Code Repository on BitBucket</a></p>
 			</div>
 		</div>
-		<div class="row">
-			<div class="col-sm-5 col-md-4 vcenter img_row">
+		<hr />
+		<div class="row double_row second_row">
+			<div class="img_row">
 				<img src="images/jBaquery_screenshot.png" alt="jBaquery" />
 			</div>
-			<div class="col-sm-7 col-md-8 vcenter text_row">
+			<div class="text_row">
 				<h2>jBaquery</h2>
 				<p>I built this site to fulfill the final requirement of an interactive web development certificate program at my community college. I utilized jQuery plugins for the home page carousel and image galleries, as well as to validate the contact form.</p>
 				<p>This responsive site, for a fictional bakery, is built on HTML, CSS, JavaScript, and jQuery.</p>

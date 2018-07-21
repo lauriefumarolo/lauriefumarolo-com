e10f1d2770530bd780eaab78f08bf1910c5dfc2d
Laurie Fumarolo <pwdlaurie@gmail.com>
2018-07-21 12:58:17 -0500

    Clean up, add horizontal divider on mobile

--- publish/index.php
+++ publish/index.php
@@ -16,8 +16,8 @@
 	<?php include 'icon_defs.php' ?>
 	<?php include 'header.php' ?>
 	<div class="container" role="main">
-		<div class="row">
-			<div class="col">
+		<div class="cards">
+			<div class="card">
 				<img src="images/kohapets_screenshot.png" alt="Koha Pets" />
 				<h2>Koha Pets</h2>
 				<p>I was independently contracted as a BigCommerce developer to add new features to this existing site based on the client's requirements.</p>
@@ -25,7 +25,7 @@
 				<p>This is a BigCommerce site and utilizes BigCommerce's Stencil API.</p>
 				<p><a href="https://kohapet.com/" target="_blank">View the Site</a></p>
 			</div>
-			<div class="col">
+			<div class="card">
 				<img src="images/rca_screenshot.png" alt="RCA Modular" />
 				<h2>Robotic Construction Automation, LLC</h2>
 				<p>I implemented this site for RCA, LLC, a company that will be offering a new concept for constructing multi-story, multi-dwelling facilities. I was hired as a web developer to build this new site based on the client's design.</p>
@@ -33,9 +33,7 @@
 				<p>This responsive site is built on HTML, CSS, JavaScript, jQuery, and Bootstrap.</p>
 				<p><a href="https://www.rca-modular.com" target="_blank">View the Site</a></p>
 			</div>
-		</div>
-		<div class="row">
-			<div class="col">
+			<div class="card">
 				<img src="images/cityplanetarium_screenshot.png" alt="City Planetarium" />
 				<h2>City Planetarium</h2>
 				<p>I created this site to demonstrate my knowledge of Twitter's Bootstrap framework and other modern web design trends. Here, I have included parallax scrolling and a large image banner on the homepage. The transparent menubar over this banner makes quite a statement. To maintain readability, however, I have written JavaScript to remove the effect as the user scrolls down the page.</p>
@@ -43,7 +41,7 @@
 				<p><a href="http://cityplanetarium.lauriefumarolo.com" target="_blank">View the Site</a></p>
 				<p><a href="http://bitbucket.org/lcaires2/cityplanetarium" target="_blank">View the Code Repository on BitBucket</a></p>
 			</div>
-			<div class="col">
+			<div class="card">
 				<img src="images/jBaquery_screenshot.png" alt="jBaquery" />
 				<h2>jBaquery</h2>
 				<p>I built this site to fulfill the final requirement of an interactive web development certificate program at my community college. I utilized jQuery plugins for the home page carousel and image galleries, as well as to validate the contact form.</p>
